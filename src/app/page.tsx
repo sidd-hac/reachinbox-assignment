4d53416099bc725e9e0954868e1f52f16405ecc5
sid <sidengineer.014@gmail.com>
2024-08-15 02:24:05 +0530

    assignment update6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const handleLogin = () => {
 
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox";
+    window.location.href = `https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=${process.env.CLIENT_URL}`;
   }
 
   return (
